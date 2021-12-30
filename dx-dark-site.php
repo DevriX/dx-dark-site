698e5bd2117cc116dda31b8ad5b667e2a8e66de2
mario9881 <mario_9881@abv.bg>
2021-12-30 12:34:56 +0200

    Apply filters

--- dx-dark-site.php
+++ dx-dark-site.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: DX Dark Site
+ * Plugin Name: DevriX Dark Site
  * Description: Plugin for emergency redirections and notices
- * Version:		1.0.1
+ * Version:		1.0.0
  * Author:      DevriX
  * Author URI:  https://devrix.com/
  * License:     GPLv2 or later
@@ -12,7 +12,7 @@
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 define( 'DX_DARKSITE_PATH', plugin_dir_path( __FILE__ ) );
-define( 'DX_STYLES_VERSION', '15102019' );
+define( 'DX_STYLES_VERSION', '20201013' );
 
 require_once( DX_DARKSITE_PATH . 'dx-menu-creation/dx-menu-creation.php' );
 
@@ -23,8 +23,15 @@ require_once( DX_DARKSITE_PATH . 'dx-menu-creation/dx-menu-creation.php' );
  */
 function dx_darksite_redirect() {
 	global $wp;
+
+	$page_id = get_the_ID();
+
+	if( has_shortcode( get_option( 'dx_my_editor' ), 'counter' ) || has_shortcode( get_post_field('post_content', $page_id), 'counter' )){
+		return;
+	}
+
 	$dx_redirect_to = get_option( 'dx_redirect_to' );
-	$dx_current_url = esc_url( home_url( add_query_arg( array( $_GET ), $wp->request ) ) );
+	$dx_current_url = home_url( add_query_arg( array( $_GET ), $wp->request ) );
 	$dx_url_with_dash = $dx_current_url . '/';
 
 	// Redirect all visitors, if the option is enabled
@@ -44,12 +51,15 @@ add_action( 'template_redirect', 'dx_darksite_redirect' );
  */
 function dx_darksite_notice() {
 	if ( ! empty( get_option( 'dx_my_editor' ) ) ) {
-		if( ! isset( $_COOKIE[ 'dx_darksite_note' ] ) ) {
+		if( ! isset( $_COOKIE['dx_darksite_note'] ) ) {
 			if( ! empty( get_option( 'dx_margin_top' ) ) ) {
 				$dx_margin_top = get_option( 'dx_margin_top' );
 			} else {
 				$dx_margin_top = 0;
 			}
+			$dx_editor_content = get_option( 'dx_my_editor' );
+			$dx_unslashed_content = wp_unslash( $dx_editor_content );
+
 			$dx_editor_content = get_option( 'dx_my_editor' );
 			$dx_date = get_option( 'dx_date' );
 			$dx_time = get_option( 'dx_time' );
@@ -58,44 +68,24 @@ function dx_darksite_notice() {
 			$dx_unslashed_date = wp_unslash( $dx_date );
 			$dx_unslashed_time = wp_unslash( $dx_time );
 
-			$dx_content = wp_kses_data( $dx_unslashed_content );
+			
 			$dx_date_kses = wp_kses_data( $dx_unslashed_date );
 			$dx_time_kses = wp_kses_data( $dx_unslashed_time );
 			$dx_date_time = $dx_date_kses . ' ' . $dx_time_kses;
-
-			$dx_custom_image = get_option( 'dx-dark-site-image' );
-
-			$expiry_date  = strtotime( $dx_date_time );
-			$current_date = strtotime( gmdate( 'Y-m-d h:i:s' ) );
-
-			$diff    = abs( $expiry_date - $current_date );
-			$years   = floor( $diff / ( 365 * 60 * 60 * 24 ) );
-			$months  = floor( ( $diff - $years * 365 * 60 * 60 * 24 ) / ( 30 * 60 * 60 * 24 ) );
-			$days    = floor( ( $diff - $years * 365 * 60 * 60 * 24 - $months * 30 * 60 * 60 * 24 ) / ( 60 * 60 * 24 ) );
-			$hours   = floor( ( $diff - $years * 365 * 60 * 60 * 24 - $months * 30 * 60 * 60 * 24 - $days * 60 * 60 * 24 ) / ( 60 * 60 ) );
-			$minutes = floor( ( $diff - $years * 365 * 60 * 60 * 24 - $months * 30 * 60 * 60 * 24 - $days * 60 * 60 * 24 - $hours * 60 * 60 ) / 60 );
-			$seconds = floor( ( $diff - $years * 365 * 60 * 60 * 24 - $months * 30 * 60 * 60 * 24 - $days * 60 * 60 * 24 - $hours * 60 * 60 - $minutes * 60 ) );
-
+			$dx_content = wp_kses_data( $dx_unslashed_content ) . ' [global-counter time="' . $dx_date_time . '"]';
 			?>
-			<?php if ( $expiry_date >= $current_date ) : ?>
 			<div class="darksite-notice">
 				<div class="darksite-notice-container">
 					<div class="darksite-notice-image">
-						<?php if( empty( $dx_custom_image ) ) { ?>
-							<img src="<?php echo plugin_dir_url( __FILE__ ) . 'assets/images/error-64-warning.png' ?>" alt="warning">
-						<?php } else { ?>
-							<img src="<?php echo $dx_custom_image; ?>" alt="warning">
-						<?php } ?>
+						<img src="<?php echo plugin_dir_url( __FILE__ ) . 'assets/images/error-64-warning.png' ?>" alt="warning">
 					</div>
-					<div class="darksite-notice-content"><?php echo apply_filters( 'the_content', $dx_content ); ?> <b><?php echo $years . ' years ' . $months . ' months '
-					. $days . ' days ' . $hours . ' hours ' . $minutes . ' minutes ' . $seconds . ' seconds'; ?></b></div>
+					<div class="darksite-notice-content"><?php echo apply_filters( 'the_content', $dx_content ); ?></div>
 					<button id="darksite-notice-button" class="darksite-notice-button" onclick="SetDarksiteCookie()"><span>+</span></button>
 				</div><!-- .darksite-notice-container -->
 			</div><!-- .darksite-notice -->
-			<?php endif; ?>
 
 			<style type="text/css">
-				.darksite-notice { margin-top: <?php echo $dx_margin_top; ?>rem; }
+				.darksite-notice { margin-top: <?php echo $dx_margin_top ?>rem; }
 			</style>
 		<?php
 		}
@@ -135,3 +125,82 @@ function dx_plugin_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'dx_plugin_scripts' );
+
+/**
+ * Counter Shortcode
+ */
+function dx_add_counter_shortcode( $atts ) {
+
+	$attributes = shortcode_atts( array(
+		'seconds' => '5',
+	), $atts );
+
+	return shortocde_handle( $attributes['seconds'] );
+}
+add_shortcode( 'counter', 'dx_add_counter_shortcode' );
+
+/**
+ * Global Counter Shortcode
+ */
+function dx_add_global_counter_shortcode( $atts ) {
+
+	$attributes = shortcode_atts( array(
+		'time' => 'January 2 2022 10:10:10',
+	), $atts );
+
+	return global_counter_shortocde_handle( $attributes['time'] );
+}
+add_shortcode( 'global-counter', 'dx_add_global_counter_shortcode' );
+
+function shortocde_handle( $atts ) { ?>
+    <script type="text/javascript">
+        var timeleft = <?php echo $atts ?>;
+        var downloadTimer = setInterval(function(){
+            if(timeleft <= 0){
+			    clearInterval(downloadTimer);
+			    document.getElementById("countdown").innerHTML = "Redirecting Now";
+				window.location.href = "<?php echo get_option( 'dx_redirect_to' ); ?>";
+            } else {
+			    document.getElementById("countdown").innerHTML = timeleft + " seconds";
+            }
+			    timeleft -= 1;
+			}, 1000);
+    </script>
+    <?php
+    return '<b id="countdown"></b>';
+}
+
+function global_counter_shortocde_handle( $atts ) {
+	?>
+	<script>
+	var countDownDate = new Date("<?php echo $atts; ?>").getTime();
+
+	var x = setInterval(function() {
+
+	  var now = new Date().getTime();
+	  var distance = countDownDate - now;
+
+	  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	  document.getElementById("counter").innerHTML = days + " days " + hours + " hours "
+	  + minutes + " months " + seconds + " seconds";
+	  if (distance < 0) {
+	    clearInterval(x);
+	    document.getElementById("demo").innerHTML = "EXPIRED";
+	  }
+	}, 1000);
+	</script>
+<?php return '<b id="counter"></b>';
+}
+
+function deactivate_this_plugin(){
+    if (get_option('dx_switch_plugin') !== 'true') {
+        deactivate_plugins('/dx-dark-site/dx-dark-site.php', true);
+    }
+    update_option('dx_switch_plugin', 'true');
+}
+add_action('admin_init', 'deactivate_this_plugin');
+
